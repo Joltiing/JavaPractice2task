7b0bd2b3ce90ddd9899b93d1a78adace29fab8ff
Joltiing <westaseast@vk.com>
2022-11-24 18:15:42 +0300

    xdJson

--- JavaPractice2/src/JsonWorker/JsonWorker.java
+++ JavaPractice2/src/JsonWorker/JsonWorker.java
@@ -6,9 +6,31 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class JsonWorker {
+    public static void InsertHumansToJson(String filePath, ArrayList<Human> humans){
+        var jsonList=new JSONArray();
+        for (var human : humans) {
+            var jsonObj=new JSONObject();
+
+            jsonObj.put("name",human._name);
+            jsonObj.put("surname",human._surname);
+            jsonObj.put("age",human._age);
+            jsonObj.put("sex",human._sex);
+
+            jsonList.add(jsonObj);
+        }
+
+        try (var file = new FileWriter(filePath)) {
+            file.write(jsonList.toJSONString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static ArrayList<Human> readPeople(String path){
         JSONParser parser = new JSONParser();
 
