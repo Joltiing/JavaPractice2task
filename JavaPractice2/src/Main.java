ae2cbd25b255dc55786bf0c10fdf07d229bfe904
Joltiing <westaseast@vk.com>
2022-11-24 18:16:36 +0300

    xdMain

--- JavaPractice2/src/Main.java
+++ JavaPractice2/src/Main.java
@@ -1,9 +1,16 @@
 import Entities.Human;
 import JsonWorker.JsonWorker;
 import XmlWorker.XmlWorker;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        var humans = XmlWorker.GetPeopleFromXml("C:\\Idea\\JavaPractice2\\src\\people.xml");
+        JsonWorker.InsertHumansToJson("C:\\Idea\\JavaPractice2\\src\\peoplejson.json",humans);
+
         var humanlist=JsonWorker.readPeople("C:\\Users\\Kokor\\IdeaProjects\\JavaPractice2task\\JavaPractice2\\src\\peoplejson.json");
 
         Human[] arr= new Human[humanlist.size()];
